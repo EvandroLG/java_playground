badba7bcc7050def96ac50f190cbe478d39acbe0
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2023-05-12 23:01:49 +0200

    implements an abstract class

--- src/main/java/inheritance/Zoo.java
+++ src/main/java/inheritance/Zoo.java
@@ -6,11 +6,21 @@ public class Zoo {
         System.out.println(rocky instanceof Animal);
         rocky.fetch();
         rocky.makeSound();
+        feed(rocky);
 
         Animal sasha = new Dog();
         sasha.makeSound();
         sasha = new Cat();
         sasha.makeSound();
         ((Cat)sasha).scratch();
+        feed(sasha);
+    }
+
+    public static void feed(Animal animal) {
+        if (animal instanceof  Dog) {
+            System.out.println("Here is your dog food!");
+        } else {
+            System.out.println("Here is your cat food!");
+        }
     }
 }
